54872c9f2c9d8db68295687f55798da9b9e7c5f6
ghwlchlaks <ghwlchlaks@naver.com>
2018-10-01 18:23:40 +0900

    running status, done status merge

--- routes/client_main.js
+++ routes/client_main.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req,res) {
+let isAuthenticated = function(req, res, next) {
+	if(req.isAuthenticated()){
+		return next()
+	}
+	res.redirect('/')
+}
+
+router.get('/',isAuthenticated, function(req,res) {
 	res.render('client_main')
 })
 module.exports = router;

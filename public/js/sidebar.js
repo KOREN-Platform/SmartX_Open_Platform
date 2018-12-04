440c56fea80c81b8210f2e4099c5fee0c230cc69
ghwlchlaks <ghwlchlaks@naver.com>
2018-12-04 14:27:15 +0900

    html indent

---
+++ public/js/sidebar.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    let url = (window.location.href).split("/")
+    $("#"+url[url.length -1]).addClass('act')
+    $("#"+url[url.length -1]).parent().addClass('show')
+  })
+
+  $('.sidebar').click(function(){
+    $('.dropdown-menu').removeClass('show')
+  })

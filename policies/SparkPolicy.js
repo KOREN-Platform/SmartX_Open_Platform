e6e47bd13c993d28c8151bf7087490e85f03358a
Kim Jumin <4qazwsx@naver.com>
2018-12-05 16:30:25 +0900

    sparkPolicy end

--- policies/SparkPolicy.js
+++ policies/SparkPolicy.js
@@ -25,50 +25,86 @@ module.exports = {
 		const submit = 'spark-submit '+'--name '+account+' '+conf.AppFolder+req.body.app_name.split('.')[0]+'/'+req.body.app_name+' --file='+req.body.file + ' ' +  req.body.parameter
 		count+=1
 
-		exec(submit, function (err, stdout, stderr) {
-			//console.log(submit)
-			if(err !== null) {
-				console.log('exec error :' + err)
-				res.send({status: false, result:"error"})
-			}
-			else {
+		const sparkSubmit = function (){
+			return new Promise(function(resolve, reject){
+				exec(submit, function (err, stdout, stderr) {
+					if(err !== null) {
+						console.log('exec error :' + err)
+						res.send({status: false, result:"error"})
+					} else{
+						resolve(stdout)
+					}
+				})
+			})
+		}
+
+		const makeResultFile = function (stdout){
+			return new Promise(function(resolve, reject){
 				console.log("count "+ count)
 				req.body.stdout = stdout
 				let file = account+count+".txt"
 				fs.writeFile(file, stdout, 'utf8', function(err){
 					if (err) {throw err}
 					else{
-						console.log("test: " +'hdfs dfs -put '+file+' result')
-						exec('hdfs dfs -put '+file+' /result', function(err, stdout, stderr){
-							if (err){
-								console.log(err)
-								throw err
-							}
-							else{
-								fs.unlink(file, function(err){
-									if(err){console.log(err)
-										throw err}
-									else{console.log("unlink test")}
-									let results = new Results({
-										application_id : count,
-										path : "result/"+file,
-										date : Date.now()
-									})
-									results.save(function(err, result){
-										if (err) {console.log(err)
-											throw err}
-										else{
-											console.log("result : " + result)
-											next()
-										}
-									})
-								})
-							}
-						})
+						resolve(file)
 					}
 				})
-			}
-		});
+			})
+		}
+
+		const resultFileUploadToHDFS = function (file){
+			return new Promise(function(resolve, reject){
+				exec('hdfs dfs -put '+file+' /result', function(err, stdout, stderr){
+					if (err){
+						console.log(err)
+						throw err
+					}
+					else{
+						resolve(file)
+					}
+				})
+			})
+		}
+
+		const deleteResultFile = function (file){
+			return new Promise(function(resolve, reject){
+				fs.unlink(file, function(err){
+					if(err){console.log(err)
+						throw err}
+					else{
+						resolve(file)
+					}
+				})
+			})
+		}
+		
+		const resultDataSaveToDB = function (file){
+			return new Promise(function(resolve, reject){
+				let results = new Results({
+					application_id : count,
+					path : "result/"+file,
+					date : Date.now()
+				})
+				results.save(function(err, result){
+					if (err) {
+						console.log(err)
+						throw err
+					}else{
+						console.log("result : " + result)
+						resolve(true)
+					}
+				})
+			})
+		}
+
+		sparkSubmit()
+		.then(stdout => makeResultFile(stdout))
+		.then(file => resultFileUploadToHDFS(file))
+		.then(file => deleteResultFile(file))
+		.then(file => resultDataSaveToDB(file))
+		.then(result => {
+			next()
+		})
 	},
 	startCount(){
 		Results.find({}, function(err, results){

ae376beedf732d9a0b2c38db5694937ab4565ec2
Kim Jumin <4qazwsx@naver.com>
2018-12-05 15:48:21 +0900

    AdminCotroller end

--- policies/AdminControllerServices.js
+++ policies/AdminControllerServices.js
@@ -19,218 +19,247 @@ module.exports = {
 	saveInfo(req, res) {
 		const body = req.info
 		const info = JSON.parse(fs.readFileSync(conf.AppFolder+body.split('.')[0]+'/'+body,'utf8'))
-		App.findOne({"appName": info.appName}, function(err, app){
-			if(err) {res.send({status:false, result: err})}
-			if(!app){
-			Users.findOne({email: req.user.email}, function(err, user){
-				if(err) {res.send({status:false, result:err})}
-				if(!user) {res.send({status:false,result:"not exists"})}
-				else{
-					let parameters = []
 
-					for (let data of info.parameters){
-						parameters.push(data)
+		
+
+		//swagger json parameters add
+		let apiParameters = ''
+		let propertiesParams = ''
+		for( let i = 0 ; info.parameters.length > i ; i++ ){
+			apiParameters+= info.parameters[i].name + ' ' + info.parameters[i].default + ' '
+			propertiesParams +=
+			'"'+info.parameters[i].name+'": {\n'+
+			'"type": "string",\n'+
+			'"example": "'+info.parameters[i].default+'",\n'+
+			'"description": "'+info.parameters[i].description+'"\n'
+			//last cut
+			if(info.parameters.length-1 == i){
+				propertiesParams += '}\n'
+			}else{
+				propertiesParams += '},\n'
+			}
+		}
+		//swagger json data
+		let	content = '{\n'+
+			'"swagger": "2.0",\n'+
+			'"info": {\n'+
+				'"description": "'+info.description+'",\n'+
+				'"version": "'+info.version+'",\n'+
+				'"title": "'+info.appName.split('.')[0]+'",\n'+
+				'"termsOfService": "http://swagger.io/terms/",\n'+
+				'"contact": {\n'+
+				'"email": "'+info.author.email+'"\n'+
+				'},\n'+
+				'"license": {\n'+
+				'"name": "Apache-2.0",\n'+
+				'"url": "http://www.apache.org/licenses/LICENSE-2.0.html"\n'+
+				'}\n'+
+			'},\n'+
+			'"host": "'+conf.HostName+':3000",\n'+
+			'"basePath": "/api/v2",\n'+
+			'"tags": [\n'+
+				'{\n'+
+				'"name": "'+info.appName.split('.')[0]+'",\n'+
+				'"description": "'+info.description+'",\n'+
+				'"externalDocs": {\n'+
+					'"description": "Find out more",\n'+
+					'"url": "http://'+conf.ManagerIp+':3000"\n'+
+				'}\n'+
+				'}\n'+
+			'],\n'+
+			'"schemes": [\n'+
+				'"http"\n'+
+			'],\n'+
+			'"paths": {\n'+
+				'"/'+info.appName.split('.')[0]+'": {\n'+
+				'"post": {\n'+
+					'"tags": [\n'+
+					'"sparkApps"\n'+
+					'],\n'+
+					'"summary": "'+info.description+'",\n'+
+					'"description": "'+info.description+'",\n'+
+					'"operationId": "'+info.appName.split('.')[0]+'",\n'+
+					'"consumes": [\n'+
+					'"application/json",\n'+
+					'"application/xml"\n'+
+					'],\n'+
+					'"produces": [\n'+
+					'"application/xml",\n'+
+					'"application/json"\n'+
+					'],\n'+
+					'"parameters": [\n'+
+					'{\n'+
+						'"in": "body",\n'+
+						'"name": "body",\n'+
+						'"description": "Arguments for running the app",\n'+
+						'"required": true,\n'+
+						'"schema": {\n'+
+						'"$ref": "#/definitions/JSON"\n'+
+						'}\n'+
+					'}\n'+
+					'],\n'+
+					'"responses": {\n'+
+					'"200": {\n'+
+						'"description": "successful operation"\n'+
+					'},\n'+
+					'"400": {\n'+
+						'"description": "Invalid status value"\n'+
+					'},\n'+
+					'"404": {\n'+
+						'"description": "not found"\n'+
+					'},\n'+
+					'"500": {\n'+
+						'"description": "server error"\n'+
+					'}\n'+
+					'},\n'+
+					'"x-swagger-router-controller": "Spark"\n'+
+				'}\n'+
+				'}\n'+
+			'},\n'+
+			'"definitions": {\n'+
+				'"Spark": {\n'+
+				'"type": "object",\n'+
+				'"required": [\n'+
+					'"account",\n'+
+					'"file",\n'+
+					'"parameter",\n'+
+					'"app_name"\n'+
+				'],\n'+
+				'"properties": {\n'+
+					'"account": {\n'+
+					'"type": "string",\n'+
+					'"example": "'+req.user.email+'",\n'+
+					'"description": "issuer\'s account(email)"\n'+
+					'},\n'+
+					'"app_name": {\n'+
+					'"type": "string",\n'+
+					'"example": "'+info.appName+'",\n'+
+					'"description": "app name"\n'+
+					'},\n'+
+					'"file": {\n'+
+					'"type": "string",\n'+
+					'"example": "AtoZ.txt",\n'+
+					'"description": "target file name"\n'+
+					'},\n'+
+					'"callback_method": {\n'+
+					'"type": "string",\n'+
+					'"example": "email",\n'+
+					'"description": "callback target (email or slack)"\n'+
+					'},\n'+
+					'"callback_addr": {\n'+
+					'"type": "string",\n'+
+					'"example": "Your@email.com",\n'+
+					'"description": "callback address"\n'+
+					'},\n'+
+					propertiesParams +
+				'},\n'+
+				'"title": "A spark",\n'+
+				'"description": "running spark",\n'+
+				'"example": {\n'+
+					'"account": "'+req.user.email+'",\n'+
+					'"app_name": "'+info.appName+'",\n'+
+					'"file": "AtoZ.txt",\n'+
+					'"parameter": "'+apiParameters+'",\n'+
+					'"callback_method": "email",\n'+
+					'"callback_addr": "Your@email.com"\n'+
+				'},\n'+
+				'"xml": {\n'+
+					'"name": "Spark"\n'+
+				'}\n'+
+				'}\n'+
+			'},\n'+
+			'"externalDocs": {\n'+
+				'"description": "Find out more about Swagger",\n'+
+				'"url": "http://swagger.io"\n'+
+			'}\n'+
+			'}'
+
+		const checkDBExist = function (){
+			return new Promise(function(resolve, reject){
+				App.findOne({"appName": info.appName}, function(err, app){
+					if(err) {
+						res.send({status:false, result:'MongoDB findOne Failed'})
 					}
-					console.log(parameters)
-					let app = new App({
-						appName : info.appName,
-						description : info.description,
-						author : info.author,
-						parameters : parameters,
-						version : info.version,
-						type : info.type,
-						user : user._id
-					})
-					app.save(function(err, result){
-						if(err) {res.send({status:false, result:err})}
-						if(result) {
-							user.apps.push(app)
-							user.save(function(err, data){
-								if(err) {res.send({status:false, result:err})}
-								if(data){
-									//res.send({status:true, result:data})
-									console.log("write swagger json")
-
-									let apiParameters = ''
-									let propertiesParams = ''
-
-									for( let i = 0 ; info.parameters.length > i ; i++ ){
-										apiParameters+= info.parameters[i].name + ' ' + info.parameters[i].default + ' '
-										propertiesParams +=
-										'"'+info.parameters[i].name+'": {\n'+
-										'"type": "string",\n'+
-										'"example": "'+info.parameters[i].default+'",\n'+
-										'"description": "'+info.parameters[i].description+'"\n'
-										//last cut
-										if(info.parameters.length-1 == i){
-											propertiesParams += '}\n'
-										}else{
-											propertiesParams += '},\n'
-										}
-									}
-									let	content = '{\n'+
-										'"swagger": "2.0",\n'+
-										'"info": {\n'+
-										  '"description": "'+info.description+'",\n'+
-										  '"version": "'+info.version+'",\n'+
-										  '"title": "'+info.appName.split('.')[0]+'",\n'+
-										  '"termsOfService": "http://swagger.io/terms/",\n'+
-										  '"contact": {\n'+
-											'"email": "'+info.author.email+'"\n'+
-										  '},\n'+
-										  '"license": {\n'+
-											'"name": "Apache-2.0",\n'+
-											'"url": "http://www.apache.org/licenses/LICENSE-2.0.html"\n'+
-										  '}\n'+
-										'},\n'+
-										'"host": "'+conf.HostName+':3000",\n'+
-										'"basePath": "/api/v2",\n'+
-										'"tags": [\n'+
-										  '{\n'+
-											'"name": "'+info.appName.split('.')[0]+'",\n'+
-											'"description": "'+info.description+'",\n'+
-											'"externalDocs": {\n'+
-											  '"description": "Find out more",\n'+
-											  '"url": "http://'+conf.ManagerIp+':3000"\n'+
-											'}\n'+
-										  '}\n'+
-										'],\n'+
-										'"schemes": [\n'+
-										  '"http"\n'+
-										'],\n'+
-										'"paths": {\n'+
-										  '"/'+info.appName.split('.')[0]+'": {\n'+
-											'"post": {\n'+
-											  '"tags": [\n'+
-												'"sparkApps"\n'+
-											  '],\n'+
-											  '"summary": "'+info.description+'",\n'+
-											  '"description": "'+info.description+'",\n'+
-											  '"operationId": "'+info.appName.split('.')[0]+'",\n'+
-											  '"consumes": [\n'+
-												'"application/json",\n'+
-												'"application/xml"\n'+
-											  '],\n'+
-											  '"produces": [\n'+
-												'"application/xml",\n'+
-												'"application/json"\n'+
-											  '],\n'+
-											  '"parameters": [\n'+
-												'{\n'+
-												  '"in": "body",\n'+
-												  '"name": "body",\n'+
-												  '"description": "Arguments for running the app",\n'+
-													'"required": true,\n'+
-												  '"schema": {\n'+
-													'"$ref": "#/definitions/JSON"\n'+
-													'}\n'+
-												'}\n'+
-												'],\n'+
-											  '"responses": {\n'+
-												'"200": {\n'+
-												  '"description": "successful operation"\n'+
-												'},\n'+
-												'"400": {\n'+
-												  '"description": "Invalid status value"\n'+
-												'},\n'+
-												'"404": {\n'+
-												  '"description": "not found"\n'+
-												'},\n'+
-												'"500": {\n'+
-												  '"description": "server error"\n'+
-												'}\n'+
-											  '},\n'+
-											  '"x-swagger-router-controller": "Spark"\n'+
-											'}\n'+
-										  '}\n'+
-										'},\n'+
-										'"definitions": {\n'+
-										  '"Spark": {\n'+
-											'"type": "object",\n'+
-											'"required": [\n'+
-											  '"account",\n'+
-											  '"file",\n'+
-											  '"parameter",\n'+
-											  '"app_name"\n'+
-											'],\n'+
-											'"properties": {\n'+
-											  '"account": {\n'+
-												'"type": "string",\n'+
-												'"example": "'+req.user.email+'",\n'+
-												'"description": "issuer\'s account(email)"\n'+
-												'},\n'+
-												'"app_name": {\n'+
-												'"type": "string",\n'+
-												'"example": "'+info.appName+'",\n'+
-												'"description": "app name"\n'+
-												'},\n'+
-											  '"file": {\n'+
-												'"type": "string",\n'+
-												'"example": "AtoZ.txt",\n'+
-												'"description": "target file name"\n'+
-											  '},\n'+
-											  '"callback_method": {\n'+
-												'"type": "string",\n'+
-												'"example": "email",\n'+
-												'"description": "callback target (email or slack)"\n'+
-											  '},\n'+
-											  '"callback_addr": {\n'+
-												'"type": "string",\n'+
-												'"example": "Your@email.com",\n'+
-												'"description": "callback address"\n'+
-											  '},\n'+
-												propertiesParams +
-											'},\n'+
-											'"title": "A spark",\n'+
-											'"description": "running spark",\n'+
-											'"example": {\n'+
-												'"account": "'+req.user.email+'",\n'+
-												'"app_name": "'+info.appName+'",\n'+
-											  '"file": "AtoZ.txt",\n'+
-											  '"parameter": "'+apiParameters+'",\n'+
-											  '"callback_method": "email",\n'+
-											  '"callback_addr": "Your@email.com"\n'+
-											'},\n'+
-											'"xml": {\n'+
-											  '"name": "Spark"\n'+
-											'}\n'+
-										  '}\n'+
-										'},\n'+
-										'"externalDocs": {\n'+
-										  '"description": "Find out more about Swagger",\n'+
-										  '"url": "http://swagger.io"\n'+
-										'}\n'+
-									  '}'
-
-									let file = conf.JsonFolder+info.appName.split('.')[0] + ".json"
-									fs.writeFile(file, content, 'utf8', function(err){
-										if(err) {res.send({status:false, result:err})}
-										else{
-											const submit = 'java -jar swagger-codegen-cli.jar generate -i '+conf.JsonFolder+ info.appName.split('.')[0]+'.json'+ ' -l python -o '+conf.SwaggerFolder+info.appName.split('.')[0]
-											exec(submit, function(err, stdout, stderr){
-												if(err){
-													console.log("1" + err)
-													res.send({status:false, result:err})
-												} else{
-													
-													zipper.sync.zip(conf.SwaggerFolder+info.appName.split('.')[0]).compress().save(conf.SwaggerFolder+info.appName.split('.')[0]+".zip")
-
-													res.send({status:true, result:stdout})
-
-												}
-											})
-										}	
-									})
-								}
-							})
-						}
-					})
-				}
+					if(!app){
+						Users.findOne({email: req.user.email}, function(err, user){
+							if(err) {
+								res.send({status:false, result:'MongoDB findOne Failed'})
+							}
+							if(!user) {
+								res.send({status:false, result:"not exists"})
+							}else{
+								resolve(user)
+							}
+						})
+					}else{
+						res.send({status:false, result: 'file exists'})
+					}
+				})
 			})
 		}
-		else{
-				res.send({status: false, result: "file exists"})
-			}
+
+		const saveDB = function (user){
+			return new Promise(function(resolve, reject){
+
+				let parameters = []
+
+				for (let data of info.parameters){
+					parameters.push(data)
+				}
+
+				let app = new App({
+					appName : info.appName,
+					description : info.description,
+					author : info.author,
+					parameters : parameters,
+					version : info.version,
+					type : info.type,
+					user : user._id
+				})
+
+				app.save(function(err, result){
+					if(err) {
+						res.send({status:false, result:'MongoDB app save Failed'})
+					}else{
+						user.apps.push(app)
+						user.save(function(err, data){
+							if(err) {
+								res.send({status:false, result:'MongoDB user save Failed'})
+							}else{
+								resolve(true)
+							}
+						})
+					}
+				})
+			})
+		}
+
+		const swaggerControll = function (){
+			return new Promise(function(resolve, reject){
+				console.log("write swagger json")
+				let file = conf.JsonFolder+info.appName.split('.')[0] + ".json"
+				fs.writeFile(file, content, 'utf8', function(err){
+					if(err) {res.send({status:false, result:err})}
+					else{
+						const submit = 'java -jar swagger-codegen-cli.jar generate -i '+conf.JsonFolder+ info.appName.split('.')[0]+'.json'+ ' -l python -o '+conf.SwaggerFolder+info.appName.split('.')[0]
+						exec(submit, function(err, stdout, stderr){
+							if(err){
+								res.send({status:false, result:err})
+							} else{
+								zipper.sync.zip(conf.SwaggerFolder+info.appName.split('.')[0]).compress().save(conf.SwaggerFolder+info.appName.split('.')[0]+".zip")
+								resolve(stdout)
+							}
+						})
+					}	
+				})
+			})
+		}
+		
+		checkDBExist()
+		.then(user => saveDB(user))
+		.then(result => swaggerControll())
+		.then(result => {
+			res.send({status:true, result:result})
 		})
 	},
 	
@@ -246,48 +275,66 @@ module.exports = {
 		const form = new multiparty.Form({
 			autoFiles: false,
 			uploadDir: conf.AppFolder,
-		});
+		})
+		
 		form.parse(req, function(error, fields, files){
 			let path = files.appFile[0].path
 			let originalName = files.appFile[0].originalFilename
 			let splitName = originalName.split('.')[0]
-			// console.log(path)
-			// console.log(originalName)
-			// console.log(splitName)
-			
-			fs.mkdir(conf.AppFolder+splitName, function(err){
-				if(err) {
-					fs.unlink(path, function(err){
-						if(!err){
-							fs.unlink(files.appFile[1].path, function(err){
-								if(!err){
-									res.send({status: false, result: "file exists"})
-								} else{
-									res.send({status:false, result: err})
-								}
-							})
-						}
-						else{
-							res.send({status:false, result: err})		
+
+			const unlink = function (){
+				return new Promise(function(resolve, reject){
+					fs.unlink(path, function(err) {
+						if(err) {
+							res.send({status: false, result: "unlink Failed"})
+						}else{
+							resolve(true)
 						}
 					})
-				}
-				else {
+				})
+			}
+
+			const makeAppFolder = function (){
+				return new Promise(function(resolve, reject){
+					fs.mkdir(conf.AppFolder+splitName, function(err){
+						if(err) {
+							unlink()
+							.then(result => {
+								path = files.appFile[1].path
+								unlink()
+							})
+							.then(result => {
+								res.send({status: false, result: "file exists"})
+							})
+						}else{
+							resolve(true)
+						}
+					})
+				})
+			}
+
+			const rename = function (){
+				return new Promise(function(resolve, reject){
 					fs.rename(path, conf.AppFolder+splitName+'/'+originalName, function(err){
-						if(err) {res.send({status: false, result: err})}
-						else {
-							path = files.appFile[1].path
-							originalName = files.appFile[1].originalFilename
-							fs.rename(path, conf.AppFolder+splitName+'/'+originalName, function(err) {
-								if(err) {res.send({status:false, result:err})}
-								else {
-									req.info = originalName
-									next()
-								}
-							})
+						if(err) {
+							res.send({status: false, result: "rename Failed"})
+						}else{
+							resolve(true)
 						}
 					})
-				}
+				})
+			}
+
+			makeAppFolder()
+			.then(result => rename())
+			.then(result => {
+				path = files.appFile[1].path
+				originalName = files.appFile[1].originalFilename
+				rename()
+			})
+			.then(result => {
+				req.info = originalName
+				next()
 			})
 		})
 	},
@@ -432,74 +479,12 @@ module.exports = {
 		})
 		.then(result => {
 			path = conf.SwaggerFolder+id+'.json'
+			unlink()
 		})
 		.then(result => removeDBData())
 		.then(result => {
 			res.send({status: true, result: result})
-		})
-
-	// 	fs.exists(path, function(appExists) {
-	// 		if(!appExists) {res.send({status: false, result: "not exists"})}
-	// 		else {
-	// 			fs.unlink(path, function(err){
-	// 				if(err) {res.send({status: false, result: "permission denied"})}
-	// 				else {
-	// 					 path = conf.AppFolder+id+'/'+id+".json"
-	// 					 fs.exists(path, function(jsonExists){
-	// 					 	if(!jsonExists) {res.send({status: false, result: "not exists"})}
-	// 					 	else {
-	// 					 		fs.unlink(path, function(err) {
-	// 					 			if(err) {res.send({status: false, result: "permission denied"})}
-	// 					 			else {
-	// 									path = conf.AppFolder+id
-	// 									fs.rmdir(path, function(err){
-	// 										if(err) {res.send({status: false, result: "permission denied"})}
-	// 										else{
-	// 											App.findOneAndRemove({appName : id+'.py'}, function(err, app) {
-	// 												if(err) throw err
-	// 												if(app){
-	// 													console.log("findOneAndRemove"+ app)
-	// 													Users.update(
-	// 														{"apps" : app._id},
-	// 														{"$pull" : {"apps" : app._id}},
-	// 														function(err, result){
-	// 															if(err) throw err
-	// 															if (result){
-	// 																exec('rm -r '+conf.SwaggerFolder+id, function(err, stdout, stderr){
-	// 																	if(err){
-	// 																		res.send({status: false, result: 'rmdir err'})
-	// 																	} else {
-	// 																		fs.unlink(conf.SwaggerFolder+id+'.zip',function(err){
-	// 																			if(err){
-	// 																				res.send({status: false, result: 'unlink err'})
-	// 																			}else{
-	// 																				fs.unlink(conf.SwaggerFolder+id+'.json',function(err){
-	// 																					if(err){
-	// 																						res.send({status: false, result: 'unlink err'})
-	// 																					} else{
-	// 																						res.send({status: true, result: result})
-	// 																					}
-	// 																				})
-	// 																			}
-	// 																		})
-	// 																	}
-	// 																})
-	// 															}
-	// 														}
-	// 													)
-	// 												}
-	// 											})				
-	// 										}
-	// 									})
-	// 								 }
-	// 							})
-	// 						}
-	// 					})
-	// 				}
-	// 			}) 
-	// 		}
-	// 	})
-		
+		})		
 	}
 }
 
